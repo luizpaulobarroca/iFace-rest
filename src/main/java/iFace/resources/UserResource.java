9998d5aef4c06cd0d3e5fd6817f83c3c677a0d50
Luiz Paulo Barroca <luizpaulobarroca@gmail.com>
2016-08-10 08:36:42 -0300

    accepting friendRequest

--- src/main/java/iFace/resources/UserResource.java
+++ src/main/java/iFace/resources/UserResource.java
@@ -41,6 +41,30 @@ public class UserResource {
         return Response.ok(users).build();
     }
 
+    @POST
+    @Path("/{id}/accept/{id2}")
+    @UnitOfWork
+    public Response acceptFriend(@PathParam("id") Long id, @PathParam("id2") Long id2) {
+        User user1 = userDao.retrive(id);
+        User user2 = userDao.retrive(id2);
+
+        if(!user1.getFriendRequest().contains(user2)) {
+            return Response.noContent().build();
+        }
+
+        List<User> friendslist = user1.getFriends();
+        friendslist.add(user2);
+        user1.setFriends(friendslist);
+
+        friendslist = user1.getFriendRequest();
+        friendslist.remove(user2);
+        user1.setFriendRequest(friendslist);
+
+        User userIn = userDao.create(user1);
+
+        return Response.ok(userIn).build();
+    }
+
     @DELETE
     @Path("/{id}")
     @UnitOfWork
